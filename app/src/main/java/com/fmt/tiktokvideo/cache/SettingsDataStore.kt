0dba5553eb4f086991879749087db51cf1b7ea3c
fangmingtao@heytea.com <fangmingtao@heytea.com>
2025-12-20 21:15:00 +0800

    feat: 使用属性代理重构 SharedPreferences 进行读写

--- app/src/main/java/com/fmt/tiktokvideo/cache/SettingsDataStore.kt
+++ app/src/main/java/com/fmt/tiktokvideo/cache/SettingsDataStore.kt
@@ -1,8 +1,11 @@
 package com.fmt.tiktokvideo.cache
 
+import androidx.core.content.edit
 import androidx.security.crypto.EncryptedSharedPreferences
 import androidx.security.crypto.MasterKey
 import com.fmt.tiktokvideo.AppContext
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 val masterKey: MasterKey =
     MasterKey.Builder(AppContext).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build()
@@ -12,3 +15,40 @@ val encryptedPrefs = EncryptedSharedPreferences(
     "secret_prefs",
     masterKey
 )
+
+class Preference<T>(val name: String, val default: T, val prefName: String = "default") :
+    ReadWriteProperty<Any?, T> {
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return findPreference(name)
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        putPreference(name, value)
+    }
+
+    private fun findPreference(key: String): T {
+        return when (default) {
+            is Long -> encryptedPrefs.getLong(key, default)
+            is Int -> encryptedPrefs.getInt(key, default)
+            is Float -> encryptedPrefs.getFloat(key, default)
+            is String -> encryptedPrefs.getString(key, default)
+            is Boolean -> encryptedPrefs.getBoolean(key, default)
+            else -> throw IllegalStateException("Unsupported type")
+        } as T
+    }
+
+    private fun putPreference(key: String, value: T) {
+        encryptedPrefs.edit {
+            when (value) {
+                is Long -> putLong(key, value)
+                is Int -> putInt(key, value)
+                is Float -> putFloat(key, value)
+                is String -> putString(key, value)
+                is Boolean -> putBoolean(key, value)
+                else -> throw IllegalStateException("Unsupported type")
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/fmt/tiktokvideo/http/AuthInterceptor.kt
+++ app/src/main/java/com/fmt/tiktokvideo/http/AuthInterceptor.kt
@@ -1,8 +1,8 @@
 package com.fmt.tiktokvideo.http
 
 import android.text.TextUtils
+import com.fmt.tiktokvideo.cache.Preference
 import com.fmt.tiktokvideo.cache.UserManager
-import com.fmt.tiktokvideo.cache.encryptedPrefs
 import okhttp3.Interceptor
 import okhttp3.Response
 
@@ -13,7 +13,7 @@ class AuthInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
         val builder = originalRequest.newBuilder()
-        val token = encryptedPrefs.getString("api_token", "")
+        val token by Preference("api_token", "")
         if (!TextUtils.isEmpty(token)) {
             builder.header("Authorization", "Bearer $token")
         }
